03a34362bca464aadf85735467555a702ee0a36d
androidnews <11444165+androidnews@user.noreply.gitee.com>
2023-08-12 05:32:39 +0000

    8.2.4s版本更新，调整个别翻译名词
    Signed-off-by: androidnews <11444165+androidnews@user.noreply.gitee.com>

---
+++ 8.2.4s/www/sqe-config.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * moOde audio player (C) 2014 Tim Curtis
+ * http://moodeaudio.org
+ *
+ * This Program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This Program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * 汉化： Androidnews
+ */
+
+require_once __DIR__ . '/inc/alsa.php';
+require_once __DIR__ . '/inc/common.php';
+require_once __DIR__ . '/inc/session.php';
+require_once __DIR__ . '/inc/sql.php';
+
+$dbh = sqlConnect();
+phpSession('open');
+
+if (isset($_POST['save']) && $_POST['save'] == '1') {
+	foreach ($_POST['config'] as $key => $value) {
+		if ($key == 'AUDIODEVICE') {
+			$value = $_SESSION['cardnum'];
+		}
+		sqlUpdate('cfg_sl', $dbh, $key, SQLite3::escapeString($value));
+	}
+
+	submitJob('slcfgupdate', '', '设置已更新', ($_SESSION['slsvc'] == '1' ? 'Squeezelite已重启' : ''));
+}
+
+phpSession('close');
+
+$result = sqlRead('cfg_sl', $dbh);
+$cfgSL = array();
+
+foreach ($result as $row) {
+	$cfgSL[$row['param']] = $row['value'];
+}
+
+$dev = getAlsaDeviceNames();
+
+$_sl_select['renderer_name'] = $cfgSL['PLAYERNAME'];
+$_sl_select['alsa_params'] = $cfgSL['ALSAPARAMS'];
+$_sl_select['output_buffers'] = $cfgSL['OUTPUTBUFFERS'];
+$_sl_select['task_priority'] = $cfgSL['TASKPRIORITY'];
+$_sl_select['audio_codecs'] = $cfgSL['CODECS'];
+$_sl_select['other_options'] = htmlentities($cfgSL['OTHEROPTIONS']);
+
+waitWorker(1, 'sqe_config');
+
+$tpl = "sqe-config.html";
+$section = basename(__FILE__, '.php');
+storeBackLink($section, $tpl);
+
+include('header.php');
+eval("echoTemplate(\"" . getTemplate("templates/$tpl") . "\");");
+include('footer.min.php');
